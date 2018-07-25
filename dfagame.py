2f6c496ecb9536f066650daf08978c4dad710bc1
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-07-25 21:18:06 +0200

    minor fix in dfagame, still problem when importing parse_dot() method

--- dfagame.py
+++ dfagame.py
@@ -12,17 +12,17 @@ args_parser.add_argument('<goal_formula>', help='Goal formula G -- MANDATORY')
 
 params = vars(args_parser.parse_args())
 pddl_parser = MyParser()
-if not params['planning_domain']:
+if not params['<planning_domain>']:
     print('Planning domain required!')
     exit()
 else:
     domain_path = params['planning_domain']
-if not params['planning_problem']:
+if not params['<planning_problem>']:
     print('Planning problem required!')
     exit()
 else:
     problem_path = params['planning_problem']
-if not params['goal_formula']:
+if not params['<goal_formula>']:
     print('Formula argument required!')
     exit()
 else:
@@ -32,7 +32,7 @@ try:
     with open(domain_path, 'r') as f:
         domain = f.read()
         f.close()
-    parsed_domain = parser(domain)
+    parsed_domain = pddl_parser(domain)
 except:
     raise ValueError('[ERROR] - Could not parse domain')
 
@@ -40,7 +40,7 @@ try:
     with open(problem_path, 'r') as f:
         problem = f.read()
         f.close()
-    parsed_problem = parser(problem)
+    parsed_problem = pddl_parser(problem)
 except:
     raise ValueError('[ERROR] - Could not parse problem')
 
@@ -60,14 +60,6 @@ dfa_automaton = parse_dot("automa.dot")
 operator_trans = dfa_automaton.create_operator_trans()
 new_domain = parsed_domain.get_new_domain(dfa_automaton.used_alpha, dfa_automaton.states, operator_trans)
 print(new_domain)
-
-# parsed_problem = parser(problem) --> getting initial state of pddl problem
-
-
-# use LTLf2DFA tool for parsing the formula --> there will be a .dot file in the main folder
-
-# parsed_automa = parse_dot('automa.dot')
-# transition_operator = parsed_automa.create_operator_trans()
 
 # transition_operator = 'trans\n:parameters ()\n:precondition (not turnDomain)\n:effect (and (when (and (q2) (not a)) (and (q2) (turnDomain))) (when (and (q2) (a)) (turnDomain)))\n'
 # states = {'1','2','3','4'}
